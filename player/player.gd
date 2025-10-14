9aa739bdbe0281d07f7b8852720b458238e16cee
Euang <xxx@xxx.com>
2025-10-14 16:35:02 +0800

    角色系统1

--- player/player.gd
+++ player/player.gd
@@ -14,11 +14,11 @@ var direction:Direction=Direction.RIGHT:
 		if not is_node_ready():await ready
 		%Direction.scale.x=direction
 
-
 func _physics_process(delta: float) -> void:
 	var input_x=Input.get_axis("a","d")
 	if not is_zero_approx(input_x):
 		direction=Direction.LEFT if input_x<0 else Direction.RIGHT
+	var is_atk_pressed=Input.is_action_just_pressed("j")
 	
 	#1/3.状态判断
 	var next_state=current_state
@@ -28,16 +28,22 @@ func _physics_process(delta: float) -> void:
 			else:next_state=State.RUN
 			if velocity.y>0:next_state=State.FALL
 			if velocity.y<0:next_state=State.RISE
+			if is_atk_pressed:next_state=State.HURT
 		State.RUN:
 			if is_zero_approx(input_x):next_state=State.IDLE
 			if velocity.y>0:next_state=State.FALL
 			if velocity.y<0:next_state=State.RISE
+			if is_atk_pressed:next_state=State.HURT
 		State.RISE:
 			if velocity.y>0:next_state=State.FALL
 			if is_on_floor():next_state=State.IDLE
+			if is_atk_pressed:next_state=State.HURT
 		State.FALL:
 			if velocity.y<0:next_state=State.RISE
 			if is_on_floor():next_state=State.IDLE
+			if is_atk_pressed:next_state=State.HURT
+		State.HURT:
+			if not %AnimationPlayer.is_playing():next_state=State.IDLE
 	#2/3.状态切换
 	if next_state==current_state:pass
 	else:
@@ -46,16 +52,21 @@ func _physics_process(delta: float) -> void:
 			State.RUN:%AnimationPlayer.play("run")
 			State.RISE:%AnimationPlayer.play("rise")
 			State.FALL:%AnimationPlayer.play("fall")
+			State.HURT:
+				%AnimationPlayer.play("hurt")
+				velocity.x=0
 		current_state=next_state
 	#3/3.状态运行
 	match current_state:
 		State.IDLE:
+			velocity.x=450*input_x
 			if Input.is_action_just_pressed("space"):velocity.y=-1000
 		State.RUN:
+			velocity.x=450*input_x
 			if Input.is_action_just_pressed("space"):velocity.y=-1000
-		State.RISE:pass
-		State.FALL:pass
+		State.RISE:velocity.x=450*input_x
+		State.FALL:velocity.x=450*input_x
+		State.HURT:pass
 	
-	velocity.x=450*input_x
 	velocity.y+=1600*delta
 	move_and_slide()
